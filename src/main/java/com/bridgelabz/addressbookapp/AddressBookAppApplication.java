c64842606cbeff4fd13c1d8cb7d083593635f5e0
Resh@308 <reshsingh1995@gmail.com>
2022-06-10 19:02:09 +0530

    created  data base

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.addressbookapp;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,7 +13,8 @@ public class AddressBookAppApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
-        log.info("AddressBook App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+        log.info("Employee Payroll DB User is {} Environment",
+                context.getEnvironment().getProperty("spring.datasource.username"));
     }
-
 }
